71a7dd7ae5d9fe3fcf158ba4209dc2a2d9a67ca3
Deep Patel <deeppatel234@gmail.com>
2019-04-07 18:03:42 +0530

    add vector icons

--- android/app/src/main/java/com/expensyapp/MainApplication.java
+++ android/app/src/main/java/com/expensyapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.expensyapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import org.pgsqlite.SQLitePluginPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new AsyncStoragePackage(),
               new SQLitePluginPackage()
       );

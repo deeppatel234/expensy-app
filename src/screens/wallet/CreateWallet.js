0f446ef88f49a5a1ac5c455cf3405daf6ec2ce39
Deep Patel <deeppatel234@gmail.com>
2019-05-05 11:49:48 +0530

    improve create wallet page

--- src/screens/wallet/CreateWallet.js
+++ src/screens/wallet/CreateWallet.js
@@ -1,51 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { Button, TextInput, View } from "react-native";
 import { Formik } from "formik";
 
 import Redux from "../../redux/ReduxRegistry";
+import TypoGraphy from "../../components/TypoGraphy";
+import Button from "../../components/Button";
+import TextInput from "../../components/TextInput";
 
+import { Container, Heading, Content } from "../../../globalStyle";
 
 const CreateWallet = ({ createWallet, history }) => (
-  <View>
-    <Formik
-      initialValues={{ name: "axis", icon: "axis", type: "bank", balance: "123.3" }}
-      onSubmit={values => {
-        values.balance = parseFloat(values.balance);
-        createWallet(values).then(() => history.goBack())
-      }}
-    >
-      {props => (
-        <View>
-          <TextInput
-            onChangeText={props.handleChange("icon")}
-            onBlur={props.handleBlur("icon")}
-            value={props.values.icon}
-          />
-          <TextInput
-            onChangeText={props.handleChange("name")}
-            onBlur={props.handleBlur("name")}
-            value={props.values.name}
-          />
-          <TextInput
-            onChangeText={props.handleChange("type")}
-            onBlur={props.handleBlur("type")}
-            value={props.values.type}
-          />
-          <TextInput
-            onChangeText={props.handleChange("balance")}
-            onBlur={props.handleBlur("balance")}
-            value={props.values.balance}
-            keyboardType="numeric"
-          />
-          <Button onPress={props.handleSubmit} title="Submit" />
-        </View>
-      )}
-    </Formik>
-  </View>
+  <Container>
+    <Heading>
+      <TypoGraphy type="heading" appearance="primary">
+        Add Wallet
+      </TypoGraphy>
+    </Heading>
+    <Content>
+      <Formik
+        onSubmit={values => {
+          values.balance = parseFloat(values.balance);
+          createWallet(values).then(() => history.goBack());
+        }}
+      >
+        {props => (
+          <React.Fragment>
+            <TextInput
+              placeholder="Select Icon"
+              onChangeText={props.handleChange("icon")}
+              onBlur={props.handleBlur("icon")}
+              value={props.values.icon}
+            />
+            <TextInput
+              placeholder="Wallet Type"
+              onChangeText={props.handleChange("type")}
+              onBlur={props.handleBlur("type")}
+              value={props.values.type}
+            />
+            <TextInput
+              placeholder="Wallet Name"
+              onChangeText={props.handleChange("name")}
+              onBlur={props.handleBlur("name")}
+              value={props.values.name}
+            />
+            <TextInput
+              placeholder="Initial Balance"
+              onChangeText={props.handleChange("balance")}
+              onBlur={props.handleBlur("balance")}
+              value={props.values.balance}
+              keyboardType="numeric"
+            />
+            <Button onPress={props.handleSubmit} text="Add" appearance="primary" />
+          </React.Fragment>
+        )}
+      </Formik>
+    </Content>
+  </Container>
 );
-
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -53,4 +65,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(CreateWallet);
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateWallet);

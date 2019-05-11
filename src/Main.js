53ba9e285e4c288d41d8dab6d3cd0f82141a2119
Deep Patel <deeppatel234@gmail.com>
2019-05-11 20:23:11 +0530

    improve code

--- src/Main.js
+++ src/Main.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react';
-import {
-  NativeRouter,
-  Route,
-  Switch,
-  BackButton,
-} from "react-router-native";
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { NativeRouter, Route, Switch, BackButton } from "react-router-native";
+import { connect } from "react-redux";
 
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
-import HomePage from './screens/homepage';
-import CreateCategory from './screens/category/CreateCategory';
-import ViewCategory from './screens/category/ViewCategory';
+import HomePage from "./screens/homepage";
+import CreateCategory from "./screens/category/CreateCategory";
+import ViewCategory from "./screens/category/ViewCategory";
 
-import CreateWallet from './screens/wallet/CreateWallet';
-import ViewWallet from './screens/wallet/ViewWallet';
+import CreateWallet from "./screens/wallet/CreateWallet";
+import ViewWallet from "./screens/wallet/ViewWallet";
 
-import CreateExpense from './screens/expense/CreateExpense';
+import CreateExpense from "./screens/expense/CreateExpense";
 
-import CurrencyModel from './screens/currency/CurrencyModel';
-
-import Redux from './redux/ReduxRegistry';
-import models from './sql/models'
+import Redux from "./redux/ReduxRegistry";
+import models from "./sql/models";
 
 class Main extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isSync: true,
+      isSync: true
     };
   }
 
   componentDidMount() {
-    const { fetchUser, fetchCategories, fetchNetwork, fetchWallets } = this.props;
+    const {
+      fetchUser,
+      fetchCategories,
+      fetchNetwork,
+      fetchWallets
+    } = this.props;
     fetchNetwork().then(() => {
       models.syncTables(false).then(() => {
         this.setState({ isSync: false });
@@ -49,7 +47,7 @@ class Main extends Component {
     const { isSync } = this.state;
 
     if (isSync) {
-       return <Text>Sync.....</Text>
+      return <Text>Sync.....</Text>;
     }
 
     if (isLoading) {
@@ -80,20 +78,29 @@ class Main extends Component {
 }
 
 // Maps state from store to props
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    isLoading: state.userLoadingStatus === 'IN_PROGRESS' || state.categoriesLoadingStatus === 'IN_PROGRESS' || state.walletsLoadingStatus === 'IN_PROGRESS',
-    isError: state.userLoadingStatus === 'ERROR' || state.categoriesLoadingStatus === 'ERROR' || state.walletsLoadingStatus === 'ERROR',
+    isLoading:
+      state.userLoadingStatus === "IN_PROGRESS" ||
+      state.categoriesLoadingStatus === "IN_PROGRESS" ||
+      state.walletsLoadingStatus === "IN_PROGRESS",
+    isError:
+      state.userLoadingStatus === "ERROR" ||
+      state.categoriesLoadingStatus === "ERROR" ||
+      state.walletsLoadingStatus === "ERROR"
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    fetchUser: () => dispatch(Redux.get('user', 'fetch')()),
-    fetchCategories: () => dispatch(Redux.get('category', 'fetch')()),
-    fetchWallets: () => dispatch(Redux.get('wallet', 'fetch')()),
-    fetchNetwork: () => dispatch(Redux.get('network', 'fetch')()),
+    fetchUser: () => dispatch(Redux.get("user", "fetch")()),
+    fetchCategories: () => dispatch(Redux.get("category", "fetch")()),
+    fetchWallets: () => dispatch(Redux.get("wallet", "fetch")()),
+    fetchNetwork: () => dispatch(Redux.get("network", "fetch")())
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main);

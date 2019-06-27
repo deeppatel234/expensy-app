2e7854f9140fdf0e75b085c805750ef3dd381575
Deep Patel <deeppatel234@gmail.com>
2019-06-27 20:08:01 +0530

    release v2.1

--- appConfig.js
+++ appConfig.js
@@ -1,3 +1,4 @@
 export const DOMAIN = '192.168.0.101';
 
-export const API_URL = `http://${DOMAIN}:5050/api/`;
+// export const API_URL = `http://${DOMAIN}:5050/api/`;
+export const API_URL = 'https://expensyapi.herokuapp.com/';
